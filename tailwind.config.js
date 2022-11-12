3ee527afe6c1c24be5c64e5920c6520d7235e7ca
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-12 22:50:37 +0600

    navbar and banner section done

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+        
+primary: "#0FCFEC",
+        
+secondary: "#19D3AE",
+        
+accent: "#3A4256",
+        
+neutral: "#1C191F",
+        
+"base-100": "#EDE9F1",
+        
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+
+}
