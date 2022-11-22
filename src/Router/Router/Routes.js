a81fc6cacd4c255b1d27d8713a07f57b22e4d4e5
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-22 21:13:23 +0600

    dashboard modified

--- src/Router/Router/Routes.js
+++ src/Router/Router/Routes.js
@@ -7,6 +7,7 @@ import MyAppointment from "../../Pages/Dashboard/MyAppointment/MyAppointment";
 import Home from "../../Pages/Home/Home/Home";
 import Login from '../../Pages/Login/Login'
 import SignUp from "../../Pages/SignUp/SignUp";
+import AdminRoute from "../AdminRoute/AdminRoute";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 export const routes = createBrowserRouter([
@@ -42,7 +43,7 @@ export const routes = createBrowserRouter([
       },
       {
         path: '/dashboard/allusers',
-        element: <AllUsers></AllUsers>
+        element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
       },
     ]
   }
